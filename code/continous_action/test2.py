affee0e9da00df90b5e5275c24855b891d1d5067
kpranav1998 <pranikhanna1998@gmail.com>
2022-04-25 18:43:21 +0530

    adding Ant NOrmal results

--- code/continous_action/test2.py
+++ code/continous_action/test2.py
@@ -1,22 +1,40 @@
-import numpy as np
+#Include this at the top of your colab code
+import os
+if not os.path.exists('.mujoco_setup_complete'):
+  # Get the prereqs
+  !apt-get -qq update
+  !apt-get -qq install -y libosmesa6-dev libgl1-mesa-glx libglfw3 libgl1-mesa-dev libglew-dev patchelf
+  # Get Mujoco
+  !mkdir ~/.mujoco
+  !wget -q https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz -O mujoco.tar.gz
+  !tar -zxf mujoco.tar.gz -C "$HOME/.mujoco"
+  !rm mujoco.tar.gz
+  # Add it to the actively loaded path and the bashrc path (these only do so much)
+  !echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin' >> ~/.bashrc 
+  !echo 'export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so' >> ~/.bashrc 
+  # THE ANNOYING ONE, FORCE IT INTO LDCONFIG SO WE ACTUALLY GET ACCESS TO IT THIS SESSION
+  !echo "/root/.mujoco/mujoco210/bin" > /etc/ld.so.conf.d/mujoco_ld_lib_path.conf
+  !ldconfig
+  # Install Mujoco-py
+  !pip3 install -U 'mujoco-py<2.2,>=2.1'
+  # run once
+  !touch .mujoco_setup_complete
 
-
-
-
-
-for i in range(1,4):
-    steps = np.load("./Normal/seed_"+str(i)+"/steps.npy")
-    reward = np.load("./Normal/seed_"+str(i)+"/reward.npy")
-
-    step_number = 1
-
-    while(steps[step_number] <= int(1.75e6)):
-        step_number += 1
-
-    reward = reward[0:step_number]
-    steps = steps[0:step_number]
-
-    np.save("./Normal/seed_"+str(i)+"/reward.npy",reward)
-    np.save("./Normal/seed_"+str(i)+"/steps.npy",steps)
-
-
+try:
+  if _mujoco_run_once:
+    pass
+except NameError:
+  _mujoco_run_once = False
+if not _mujoco_run_once:
+  # Add it to the actively loaded path and the bashrc path (these only do so much)
+  try:
+    os.environ['LD_LIBRARY_PATH']=os.environ['LD_LIBRARY_PATH'] + ':/root/.mujoco/mujoco210/bin'
+  except KeyError:
+    os.environ['LD_LIBRARY_PATH']='/root/.mujoco/mujoco210/bin'
+  try:
+    os.environ['LD_PRELOAD']=os.environ['LD_PRELOAD'] + ':/usr/lib/x86_64-linux-gnu/libGLEW.so'
+  except KeyError:
+    os.environ['LD_PRELOAD']='/usr/lib/x86_64-linux-gnu/libGLEW.so'
+  # presetup so we don't see output on first env initialization
+  import mujoco_py
+  _mujoco_run_once = True
